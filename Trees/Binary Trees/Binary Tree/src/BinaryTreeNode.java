09845864e6d8027ce826889419af1544c0e152ab
Nishyanth varun reddy Somagattu <nishyanthvarun17@gmail.com>
2024-03-01 15:18:48 -0600

    Node for Binary tree class created

---
+++ Trees/Binary Trees/Binary Tree/src/BinaryTreeNode.java
@@ -0,0 +1,10 @@
+public class BinaryTreeNode {
+    Object value;
+    BinaryTreeNode left;
+    BinaryTreeNode right;
+    public BinaryTreeNode(Object value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
