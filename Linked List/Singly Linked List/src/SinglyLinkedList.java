710868aced3ba0f761d0c05d7a3f9a3349c29a45
Nishyanth varun reddy Somagattu <nishyanthvarun17@gmail.com>
2024-03-01 12:06:50 -0600

    Singly linked list class

---
+++ Linked List/Singly Linked List/src/SinglyLinkedList.java
@@ -0,0 +1,11 @@
+public class SinglyLinkedList {
+    Node head;
+    Node tail;
+    int length;
+    public SinglyLinkedList(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+}
